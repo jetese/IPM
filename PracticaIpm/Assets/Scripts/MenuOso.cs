a333e0403f5be78d2a210760116ff0bfaae623a1
Alberto Blanco <alber09055@gmail.com>
2017-11-12 17:32:37 +0100

    Terminado primer menú

---
+++ PracticaIpm/Assets/Scripts/MenuOso.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOso : MonoBehaviour {
+
+    Animator anim;
+    new AudioSource audio;
+
+    void Start () {
+        anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+        StartCoroutine("startAudio");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!audio.isPlaying)
+        {
+            anim.SetBool("Talking", false);
+        }
+	}
+
+    IEnumerator startAudio()
+    {
+        print("Inicio");
+        yield return new WaitForSeconds(2f);
+        print("Fin");
+        yield return null;
+        playWelcome();
+    }
+
+    private void playWelcome()
+    {
+
+        anim.SetBool("Talking", true);
+        audio.Play();
+    }
+}
