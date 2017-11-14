fd668383183ccfb60d20ce2d60a076a8b6c228a1
Alberto Blanco <alber09055@gmail.com>
2017-11-14 11:49:32 +0100

    Añadida escena de nivel y movimiento del personaje por el camino

---
+++ PracticaIpm/Assets/Scripts/Ejercicio1/TileMapping.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TileMapping : MonoBehaviour {
+    List<GameObject> Tiles;
+    Animator anim;
+    int index = 0;
+    bool moving = false;
+    // Use this for initialization
+    void Start () {
+        Tiles = (from tile in GameObject.FindGameObjectsWithTag("Tile")
+                 orderby int.Parse(tile.name)
+                 select tile).ToList<GameObject>();
+        transform.localPosition = Tiles[index].transform.position;
+        anim = GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!moving)
+            {
+                index++;
+                StartCoroutine("move");
+            }
+        }
+	}
+
+    IEnumerator move()
+    {
+        moving = true;
+        anim.SetBool("Walking", true);
+
+        if (transform.localPosition.x <= Tiles[index].transform.position.x)
+        {
+            while (transform.localPosition.x <= Tiles[index].transform.position.x)
+            {
+                transform.Translate(new Vector3(0.05f, 0, 0));
+                yield return new WaitForFixedUpdate();
+
+            }
+        }
+        else if (transform.localPosition.y <= Tiles[index].transform.position.y)
+        {
+            while (transform.localPosition.y <= Tiles[index].transform.position.y)
+            {
+                transform.Translate(new Vector3(0, 0.05f, 0));
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        else if (transform.localPosition.y >= Tiles[index].transform.position.y)
+        {
+            while (transform.localPosition.y >= Tiles[index].transform.position.y)
+            {
+                transform.Translate(new Vector3(0, -0.05f, 0));
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        moving = false;
+        anim.SetBool("Walking", false);
+        yield return null;
+    }
+
+
+}
---
+++ PracticaIpm/Assets/Scripts/Ejercicio1/TileScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class TileScript : MonoBehaviour {
+
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
