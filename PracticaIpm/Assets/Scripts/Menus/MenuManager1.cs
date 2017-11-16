ab75f3185614bfb9226fe17284f7159b12b1c175
Eva <evishaemece@gmail.com>
2017-11-16 19:22:34 +0100

    nueva escena para el menu3 y cambiado el menumanager para hecerlo generico

--- PracticaIpm/Assets/Scripts/Menus/MenuManager1.cs
+++ PracticaIpm/Assets/Scripts/Menus/MenuManager1.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager1 : MonoBehaviour {
+    private string prevScene;
+    public bool isAutomatic = false;
+    public string nextScene;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +15,18 @@ public class MenuManager1 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.anyKey) {
-            SceneManager.LoadScene("Menu2");    
-
+        if (Input.anyKey && isAutomatic) {
+            SceneManager.LoadScene(nextScene);    
         }
-    } 
+    }
+
+    public void goToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void goBackScene()
+    {
+        SceneManager.LoadScene(prevScene);
+    }
 }
