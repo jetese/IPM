d76687ba2f46ad0cd82a185fba0c1e5f49ad7f93
Pepe Segade <pepest97@gmail.com>
2017-11-16 21:05:11 +0100

    Introducido un GameManager y un ButtonManager

---
+++ PracticaIpm/Assets/Scripts/Menus/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public string nextScene;
+
+    public void ClickFunction()
+    {
+        GameObject gameManager = GameObject.Find("GameManager");
+        MenuManager1 menuManager = gameManager.GetComponent<MenuManager1>();
+        menuManager.goToScene(nextScene);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
