d00f3079bbdb004445a5b94c7d3a0be699e93819
jetese <jetese2@gmail.com>
2017-11-17 01:04:39 +0100

    Suma se ve bien

--- PracticaIpm/Assets/Scripts/Ejercicio1/Suma.cs
+++ PracticaIpm/Assets/Scripts/Ejercicio1/Suma.cs
@@ -11,8 +11,10 @@ public class Suma : MonoBehaviour {
     public GameObject Erizo;
 	public GameObject canvas;
 	public GameObject suma;
+	public GameObject barra;
 	GameObject sum1;
 	GameObject sum2;
+	GameObject bar;
 	GameObject sol;
 	GameObject sol2;
 	GameObject elefante;
@@ -27,12 +29,16 @@ public class Suma : MonoBehaviour {
         Sumauno ();
 	}
 	void Sumauno(){
+		bar = Instantiate (barra) as GameObject;
+		bar.transform.position = new Vector3 (-7, 2.5f, 0);
+		bar.transform.parent = canvas.transform;
+		bar.transform.localScale = new  Vector3 (2.5f, 0.25f, 1);
 		sum1 = Instantiate (numeros[2]) as GameObject;
-		sum1.transform.position = new Vector3 (-7, 4, 0);
+		sum1.transform.position = new Vector3 (-8.5f, 3.5f, 0);
 		sum2 = Instantiate (numeros[3]) as GameObject;
-		sum2.transform.position = new Vector3 (-5, 4, 0);
+		sum2.transform.position = new Vector3 (-5.5f, 3.5f, 0);
 		GameObject sum = Instantiate (suma) as GameObject;
-		sum.transform.position = new Vector3 (-6, 4, 0);
+		sum.transform.position = new Vector3 (-7f, 3.5f, 0);
 		sum1.transform.parent = canvas.transform;
 		sum2.transform.parent = canvas.transform;
 		sum.transform.parent = canvas.transform;
@@ -45,9 +51,9 @@ public class Suma : MonoBehaviour {
 		Destroy (sum1);
 		Destroy (sum2);
 		sum1 = Instantiate (numeros[4]) as GameObject;
-		sum1.transform.position = new Vector3 (-7, 4, 0);
+		sum1.transform.position = new Vector3 (-8.5f, 3.5f, 0);
 		sum2 = Instantiate (numeros[6]) as GameObject;
-		sum2.transform.position = new Vector3 (-5, 4, 0);
+		sum2.transform.position = new Vector3 (-5.5f, 3.5f, 0);
 		sum1.transform.parent = canvas.transform;
 		sum2.transform.parent = canvas.transform;
 		sum1.transform.localScale = new  Vector3 (1, 1, 1);
@@ -59,9 +65,9 @@ public class Suma : MonoBehaviour {
 		Destroy (sum1);
 		Destroy (sum2);
 		sum1 = Instantiate (numeros[2]) as GameObject;
-		sum1.transform.position = new Vector3 (-7, 4, 0);
+		sum1.transform.position = new Vector3 (-8.5f, 3.5f, 0);
 		sum2 = Instantiate (numeros[4]) as GameObject;
-		sum2.transform.position = new Vector3 (-5, 4, 0);
+		sum2.transform.position = new Vector3 (-5.5f, 3.5f, 0);
 		sum1.transform.parent = canvas.transform;
 		sum2.transform.parent = canvas.transform;
 		sum1.transform.localScale = new  Vector3 (1, 1, 1);
@@ -77,6 +83,7 @@ public class Suma : MonoBehaviour {
                generateStars();
 				Sumados ();
 				script.pasos = 0;
+				Solucion (0);
 			} else {
 				script.Reintentar ();
                 playAudioFromList(BadAudios);
@@ -89,6 +96,7 @@ public class Suma : MonoBehaviour {
                 generateStars();
                 Sumatres ();
 				script.pasos = 0;
+				Solucion (0);
 			} else {
 				script.Reintentar ();
                 playAudioFromList(BadAudios);
@@ -98,6 +106,7 @@ public class Suma : MonoBehaviour {
 			if (pasos == 6) {
 				SceneManager.LoadScene(5);
 				script.pasos = 0;
+				Solucion (0);
 			} else {
 				script.Reintentar ();
                 playAudioFromList(BadAudios);
@@ -141,15 +150,21 @@ public class Suma : MonoBehaviour {
 		Destroy (sol);
 		Destroy (sol2);
 		if (paso >= 10) {
-			sol2 = Instantiate (numeros[1]) as GameObject;
-			sol2.transform.position = new Vector3 (-7, 3, 0);
+			sol2 = Instantiate (numeros [1]) as GameObject;
+			sol2.transform.position = new Vector3 (-7.75f, 1.5f, 0);
 			sol2.transform.parent = canvas.transform;
 			sol2.transform.localScale = new  Vector3 (1, 1, 1);
 			paso = paso % 10;
+			sol = Instantiate (numeros [paso]) as GameObject;
+			sol.transform.position = new Vector3 (-6.25f, 1.5f, 0);
+			sol.transform.parent = canvas.transform;
+			sol.transform.localScale = new  Vector3 (1, 1, 1);
+		} else {
+			sol = Instantiate (numeros[paso]) as GameObject;
+			sol.transform.position = new Vector3 (-7f, 1.5f, 0);
+			sol	.transform.parent = canvas.transform;
+			sol.transform.localScale = new  Vector3 (1, 1, 1);
 		}
-		sol = Instantiate (numeros[paso]) as GameObject;
-		sol.transform.position = new Vector3 (-7, 3, 0);
-		sol	.transform.parent = canvas.transform;
-		sol.transform.localScale = new  Vector3 (1, 1, 1);
+
 	}
 }
